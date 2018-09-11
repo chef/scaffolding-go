44e7b9693f5993b80c4c236ca214f2847ccd39ea
Salim Afiune <afiune@chef.io>
2018-09-11 12:22:30 -0400

    Apply new functionalities to go-app
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- test/go-app/habitat/plan.sh
+++ test/go-app/habitat/plan.sh
@@ -3,9 +3,20 @@ pkg_origin=chef
 pkg_description="Go Application to test Chef internal scaffolding-go wrapper"
 pkg_maintainer="Chef Software Inc. <support@chef.io>"
 pkg_version="0.1.0"
+pkg_bin_dirs=(bin)
 pkg_license=('Chef-MLSA')
 pkg_upstream_url="https://github.com/chef/scaffolding-go/test/go-app"
 # @afiune This little hack will let us use the latest installed version of the
 # scaffolding-go built with your origin so we can test it before releasing
 pkg_scaffolding=$HAB_ORIGIN/scaffolding-go
 scaffolding_go_base_path=github.com/chef
+# nested go-app in repo
+scaffolding_go_repo_name=scaffolding-go
+scaffolding_go_import_path="${scaffolding_go_base_path}/$scaffolding_go_repo_name/test/${pkg_name}"
+# multi-binary projects
+#scaffolding_go_binary_list=()
+
+do_prepare(){
+  # Inject flags to the go compiler
+  export GO_LDFLAGS="-X main.version=`date -u +%Y%m%d.%H%M%S`"
+}
