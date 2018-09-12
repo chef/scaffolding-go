af6f00591675af4bb40ecfdfb121e6eca99e22bf
Salim Afiune <afiune@chef.io>
2018-09-12 14:17:03 -0400

    Complete the single-binary go app
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- test/repo/components/single-binary/app.go
+++ test/repo/components/single-binary/app.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/chef/scaffolding-go/test/repo/libs"
+)
+
+func main() {
+	libs.VeryUsefulFunction()
+	fmt.Println("single-binary: Hola, todo bien!")
+}
