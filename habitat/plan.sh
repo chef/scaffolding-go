715914bf8e28a009bde5d2285c8247a9e5436fd2
Salim Afiune <afiune@chef.io>
2019-07-31 11:16:43 +0200

    Use core/scaffolding-go 0.2.0 (#28)
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -7,7 +7,7 @@ pkg_license=('Chef-MLSA')
 pkg_source=nosuchfile.tar.gz
 pkg_upstream_url="https://github.com/chef/scaffolding-go"
 pkg_deps=(
-  core/scaffolding-go/0.1.0
+  core/scaffolding-go
   core/grep
 )
 
@@ -31,4 +31,8 @@ do_download() {
 do_install() {
   install -D -m 0644 "$PLAN_CONTEXT/../lib/scaffolding.sh" "$pkg_prefix/lib/scaffolding.sh"
   install -D -m 0644 "$PLAN_CONTEXT/../lib/scaffolding_overrides.sh" "$pkg_prefix/lib/scaffolding_overrides.sh"
+
+  # libs from core/scaffolding-go
+  install -D -m 0644 "$(pkg_path_for core/scaffolding-go)/lib/go_module.sh" "$pkg_prefix/lib/go_module.sh"
+  install -D -m 0644 "$(pkg_path_for core/scaffolding-go)/lib/gopath_mode.sh" "$pkg_prefix/lib/gopath_mode.sh"
 }
